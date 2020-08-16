d03ada740426043a44e3e20dc7027369b4eb9ac8
Harry Tucker <harrytuckerubuntu@gmail.com>
2020-08-16 15:27:57 +0100

    Implement JSON handling, now much more robust
    Here we get to exploit some of the Rust typesystem to do the heavy
    lifting for us. Everything is much more nicely typed, and we can
    generate JSON that makes more sense than the Flask version.

--- src/form/models.rs
+++ src/form/models.rs
@@ -1,28 +1,61 @@
 use serde::{Deserialize, Serialize};
 
+/// MissionForm serves as a parent struct that wraps everything.
+///
+/// This is useful on the client side of things...
 #[derive(Serialize, Deserialize, Debug)]
 pub struct MissionForm {
     pub missions: Vec<Mission>,
 }
 
-/// Mission represents a mission for the competition
+/// The Mission type and its childen use the serde serialisation
+/// items to provide a nice JSON representation of all the missions in
+/// a given competition, and to use it in Rust nicely.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Mission {
     pub title: String,
-    pub sections: Vec<Section>,
+    pub fields: Vec<Field>,
 }
 
-/// Section represents a field type and its value (score-wise)
+/// A mission contains a number of fields, which can be one of the
+/// following types
+///
+/// StringField: effectively a form label for extra info
+/// CheckboxField: a checkbox and a text description, can give points
+/// SelectField: a drop-down with a text description
+/// RadioField: radio buttons with a text description
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Section {
-    pub field_type: FieldType,
+pub enum Field {
+    StringField(String),
+    CheckboxField(Checkbox),
+    SelectField(Select),
+    RadioField(Radio),
+}
+
+/// CheckboxField: a checkbox and a text description, can give points
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Checkbox {
+    pub text: String,
     pub value: i32,
 }
 
+/// SelectField: a drop-down with a text description
 #[derive(Serialize, Deserialize, Debug)]
-pub enum FieldType {
-    StringField(String),
-    CheckboxField,
-    SelectField,
-    RadioField,
+pub struct Select {
+    pub text: String,
+    pub choices: Vec<Choice>,
+}
+
+/// RadioField: radio buttons with a text description
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Radio {
+    pub text: String,
+    pub choices: Vec<Choice>,
+}
+
+/// Choice: generic struct for field types that have multiple choices
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Choice {
+    pub text: String,
+    pub value: i32,
 }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ extern crate rocket;
 extern crate serde;
 extern crate serde_json;
 
-use std::fs::File;
-use std::path::Path;
-
 use rocket_contrib::json::Json;
 
 mod form;
@@ -16,20 +13,38 @@ use form::models::*;
 
 #[get("/")]
 fn index() -> Json<MissionForm> {
+    let mission = Mission {
+        title: String::from("M01 - Elevated Places"),
+        fields: vec![
+            Field::CheckboxField(Checkbox {
+                text: String::from("The Robot needs to be supported by the bridge:"),
+                value: 20,
+            }),
+            Field::RadioField(Radio {
+                text: String::from(
+                    "Flag point only available if the bridge portion is successful!",
+                ),
+                choices: vec![
+                    Choice {
+                        text: String::from("No flags raised"),
+                        value: 0,
+                    },
+                    Choice {
+                        text: String::from("1 flag raised"),
+                        value: 15,
+                    },
+                    Choice {
+                        text: String::from("2 flags raised"),
+                        value: 30,
+                    },
+                ],
+            }),
+            Field::StringField(String::from("It is okay and expected for Robots to collide while trying to earn flag points. When clearly only one robot holds a raised flag, only that Robot scores for that flag."))
+        ],
+    };
+
     Json(MissionForm {
-        missions: vec![Mission {
-            title: "M01 - The Testing Range".to_string(),
-            sections: vec![
-                Section {
-                    field_type: FieldType::RadioField,
-                    value: 10,
-                },
-                Section {
-                    field_type: FieldType::StringField("KONO DIO DA".to_string()),
-                    value: 0,
-                },
-            ],
-        }],
+        missions: vec![mission],
     })
 }
 
